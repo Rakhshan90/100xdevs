39e0d74916ee1a93a96cc0b4fda24893785e96a9
Rakhshan <rakhshancoder@gmail.com>
2025-10-11 16:16:47 +0530

    updated week-24.1

--- week-24/week-24.1/src/index.ts
+++ week-24/week-24.1/src/index.ts
@@ -1,37 +1,43 @@
 import path from 'path';
 import * as grpc from '@grpc/grpc-js';
-import { GrpcObject, ServiceClientConstructor } from "@grpc/grpc-js"
 import * as protoLoader from '@grpc/proto-loader';
+import { ProtoGrpcType } from './generated/a';
+import { AddressBookServiceHandlers } from './generated/AddressBookService';
 
 const packageDefinition = protoLoader.loadSync(path.join(__dirname, '../src/a.proto'));
 
-const personProto = grpc.loadPackageDefinition(packageDefinition);
+const personProto = grpc.loadPackageDefinition(packageDefinition) as unknown as ProtoGrpcType;
 
-const PERSONS = [
-    {
-        name: "harkirat",
-        age: 45
-    },
-    {
-        name: "raman",
-        age: 45
-    },
-];
+const PERSONS: {name: string, age: number}[] = [];
 
-//@ts-ignore
-function addPerson(call, callback) {
-    console.log(call)
-    let person = {
-        name: call.request.name,
-        age: call.request.age
+const addressBookHandlers: AddressBookServiceHandlers = {
+    AddPerson: (call, callback) => {
+        console.log(call)
+
+        let person = {
+            name: call.request.name,
+            age: call.request.age,
+        }
+
+        PERSONS.push(person);
+        callback(null, person);
+    },
+
+    GetPersonByName: (call, callback) => {
+        let person = call.request.name;
+        let foundPerson = PERSONS.find(p => p.name === person);
+        console.log('foundPerson', foundPerson);
+        if(foundPerson) {
+            callback(null, foundPerson);
+        } else {
+            callback(null, {name: '', age: 0});
+        }
     }
-    PERSONS.push(person);
-    callback(null, person)
 }
 
 const server = new grpc.Server();
 
-server.addService((personProto.AddressBookService as ServiceClientConstructor).service, { addPerson: addPerson });
+server.addService((personProto.AddressBookService).service, addressBookHandlers);
 server.bindAsync('0.0.0.0:50051', grpc.ServerCredentials.createInsecure(), () => {
     server.start();
 });
